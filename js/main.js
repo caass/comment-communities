3b3fac9cd6a9a839fd26315cc725099cfa4a5141
Daniel Fridkin <danielfridkin@gmail.com>
2018-02-19 15:03:41 -0500

    Added testing
    Added a method to test to make sure your connection is working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
+// Main constructor (?)
 function getNewComments( callback ){
 
+    /*
+    * Attributes
+    */
+
+    // Boolean: Are you currently getting comments?
     this.gettingComments = false;
 
+    /*
+    * Methods
+    */
+
+    // Continuously get comments on a one-second interval so long as this.gettingComments
     this.start = async function(){
         this.gettingComments = true;
         while( this.gettingComments ){
@@ -20,8 +31,22 @@ function getNewComments( callback ){
         }
     }
 
+    // Set this.gettingComments to false, which stops .start()
     this.stop = function(){
         this.gettingComments = false;
+    }
+
+    // Get comments once to make sure everything is working
+    this.test = function(){
+        console.log('Getting new comments...');
+        d3.json('https://www.reddit.com/r/all/comments/.json?limit=100', function(error, data){
+            if(error){
+                throw error;
+            } else {
+                callback(data);
+                console.log('Got comments!');
+            }
+        });
     }
 
 }
